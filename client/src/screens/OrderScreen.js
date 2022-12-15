39c04bf9c7a6a35649dab1898c111e736ca4c909
hophan3300 <hophan3300@gmail.com>
2022-12-16 05:19:36 +0700

    fix

--- client/src/screens/OrderScreen.js
+++ client/src/screens/OrderScreen.js
@@ -34,7 +34,7 @@ const OrderScreen = ({ match }) => {
   useEffect(() => {
     const addPayPalScript = async () => {
       const { data: clientId } = await axios.get(
-        "https://shoes-ecommerce-api.onrender.com//api/config/paypal"
+        "https://shoes-ecommerce-api.onrender.com/api/config/paypal"
       );
       const script = document.createElement("script");
       script.type = "text/javascript";
